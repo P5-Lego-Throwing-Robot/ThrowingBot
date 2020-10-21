23667c7fa20cbc4123f1386c844f3f8cba025545
BSPetersson <bastianpetersson@hotmail.com>
2020-10-21 09:48:21 +0200

    catkin workspace created

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/bspetersson/Documents/P5/ThrowingBot/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/bspetersson/Documents/P5_Lego_Throwing_Robot/Code/Lego-Throwing-Robot/main_ws/devel;/opt/ros/melodic'.split(';') if '/home/bspetersson/Documents/P5_Lego_Throwing_Robot/Code/Lego-Throwing-Robot/main_ws/devel;/opt/ros/melodic' != '' else []
